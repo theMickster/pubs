2765df8ba232610974e417120f1b401467329e88
Mick Letofsky <mletofsky@outlook.com>
2019-12-13 18:43:28 -0700

    Few small changes. :-)

---
+++ src/Pubs.Console.Demo/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Pubs.Console.Demo
+{
+    using System;
+    using Autofac;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Pubs.Application.Common.Helpers;
+    using Pubs.Application.Common.Interfaces;
+    using Pubs.Infrastructure.Persistence.DbContexts;
+
+    internal class Program
+    {
+        private static IContainer Container { get; set; }
+
+        private static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Pubs Demo Console");
+
+            Container = ContainerConfiguration.ConfigureContainer();
+
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var serviceProvider = new ServiceCollection()
+                                        .AddEntityFrameworkSqlServer()
+                                        .BuildServiceProvider();
+
+                var configuration = new ConfigurationBuilder()
+                                        .AddJsonFile("appsettings.json")
+                                        .Build();
+
+                var defaultSchema = new DbContextSchemaHelper(configuration["DefaultSchema"]);
+
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+                var builder = new DbContextOptionsBuilder<PubsContext>();
+
+                builder.UseSqlServer(connectionString)
+                    .UseInternalServiceProvider(serviceProvider);
+                
+                //var context = scope.Resolve<IPubsContext>(
+                //     new NamedParameter("options", builder), new NamedParameter("schema", defaultSchema)
+                //    );
+                //var employeeRepository = scope.Resolve<IEmployeeRepository>(new NamedParameter(nameof(PubsContext), context));
+                
+
+            }
+
+
+             Console.ReadKey();
+        }
+    }
+}
--- tests/Pubs.Infrastructure.IntegrationTests/DbContexts/PubsContextTests.cs
+++ tests/Pubs.Infrastructure.IntegrationTests/DbContexts/PubsContextTests.cs
@@ -6,6 +6,7 @@ using Pubs.Application.Common.Helpers;
 using System;
 using System.Linq;
 using Xunit;
+using Pubs.CoreDomain.Entities;
 
 namespace Pubs.Infrastructure.IntegrationTests.DbContexts
 {
@@ -64,9 +65,57 @@ namespace Pubs.Infrastructure.IntegrationTests.DbContexts
 
             Assert.NotNull(data);
             Assert.Empty(data);
-
         }
 
+        [Fact]
+        public void add_job_publisher_and_employee_to_database_succeeds()
+        {
+            // Arrange
+            var jobDescription = "IntegrationTestJob";
+            var publisherName = "IntegrationTestPublisher";
+            var publisherCode = "9989";
+            var employeeCode = "emp11111F";
+
+            var job = new Job { JobDescription = jobDescription, MinimumLevel = 200, MaximumLevel = 250 };
+            var publuisher = new Publisher { PublisherName = publisherName, PublisherCode = publisherCode, City = "Aurora", State = "CO", ZipCode = "88888" };
+            var employee = new Employee {
+                EmployeeCode = employeeCode,
+                FirstName = "Integration",
+                MiddleName = "IT",
+                LastName = "Test",
+                JobLevel = 250,
+                PublisherCode = publisherCode,
+                HireDate = DateTime.Now
+            };
+
+            // Act
+            _context.Publishers.Add(publuisher);
+            _context.Jobs.Add(job);
+            _context.SaveChanges();
+
+            var newJob = _context.Jobs.FirstOrDefault(j => j.JobDescription == jobDescription);
+            var newPublisher = _context.Publishers.FirstOrDefault(p => p.PublisherName == publisherName);
+
+            employee.JobId = newJob.Id;
+            employee.PublisherId = newPublisher.Id;
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+
+            var newEmployee = _context.Employees.FirstOrDefault(e => e.EmployeeCode == employeeCode);
+
+            // Assert
+            Assert.NotNull(newEmployee);
+            Assert.NotNull(newEmployee.Publisher);
+            Assert.NotNull(newEmployee.Job);
+            Assert.Equal(job.JobDescription, newJob.JobDescription);
+            Assert.Equal(publuisher.PublisherName, newPublisher.PublisherName);
+
+            // Clean-up
+            _context.Employees.Remove(newEmployee);
+            _context.Jobs.Remove(newJob);
+            _context.Publishers.Remove(newPublisher);
+            _context.SaveChanges();
+        }
 
 
         
