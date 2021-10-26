42b53345e92bb92a09f114959a0baa05f3481638
Mick Letofsky <mletofsky@outlook.com>
2021-10-26 17:55:16 -0600

    Updating components and adding a custom pipe.

--- src/Pubs.UI.Angular/src/app/shared/shared.module.ts
+++ src/Pubs.UI.Angular/src/app/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { YesNoPipe } from './yes-no.pipe';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [
+  declarations: [  
+    YesNoPipe
   ],
   exports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    YesNoPipe
   ]
 })
 export class SharedModule { }
